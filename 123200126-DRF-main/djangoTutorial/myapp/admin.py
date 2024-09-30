b1947efc016527dd605ad34cd3807ba111a71285
ThuandeptraiUWU <107125515+ThuandeptraiUWU@users.noreply.github.com>
2024-10-01 00:25:41 +0700

    Delete 123200126-DRF-main/djangoTutorial directory

--- 123200126-DRF-main/djangoTutorial/myapp/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Category, Product, Cart, Order, OrderItem, CartItem
-# Register your models here.
-admin.site.register(Category)
-admin.site.register(Product)
-admin.site.register(Order)
-admin.site.register(Cart)
-admin.site.register(OrderItem)
-admin.site.register(CartItem)
--- 123200126-DRF-main/djangoTutorial/myapp/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-# Create your views here.
-
-def index(request):
-    return render(request, template_name='index.html', context= {
-        'name': 'Tran Ngoc Son'
-    })
